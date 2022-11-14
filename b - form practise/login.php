772833e6a534b82489f41d2231e11dffa3ebbfc5
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-11-14 08:57:45 +0200

    Update login.php

--- b - form practise/login.php
+++ b - form practise/login.php
@@ -42,6 +42,7 @@ if ($conn->error) {
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully" . "<br>";
             echo "Welcome, your application has been sucessfully submitted ".$firstname;
+
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
